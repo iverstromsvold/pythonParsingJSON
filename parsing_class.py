5bc7da8f228af3a03f0e8287a1e9d8bb8ad6e64d
Iver Strømsvold <iver.stromsvold@vegvesen.no>
2024-03-13 15:08:19 +0100

    created new functions for getting new values
    I have begun to create a couple of functions to encapsulate the class. Will continue next week.

--- parsing_class.py
+++ parsing_class.py
@@ -2,28 +2,29 @@ import json
 import requests
 
 class Recipes:
-    """def __init__(self):
-        self.id = id
-        self.n = n
-        self.ingr = ingr
-        self.instr = instr"""
-    def get_basics(a):
-        # a = 45
-        base_url = 'https://dummyjson.com/recipes?limit=' + str(a)
-        get_JSON = requests.get(base_url)
-        JSON_data = get_JSON.json()
+    def __init__(self):
+        self.a = a
+        self.base_url = 'https://dummyjson.com/recipes?limit=' + str(a)
 
+    def get_ID(a):
+        # a = 45
+        get_JSON = requests.get(self.base_url)
+        JSON_data = get_JSON.json()
         for i in range(a):
+            id = JSON_data["recipes"][i]['id']
+        return id
+
+        """for i in range(a):
             id = JSON_data["recipes"][i]['id']
             n = JSON_data["recipes"][i]['name']
             ingr = JSON_data["recipes"][i]['ingredients']
-            instr = JSON_data["recipes"][i]['instructions']
+            instr = JSON_data["recipes"][i]['instructions']"""
 
-            print('id: ', id)
-            print('name: ', n)
-            print('ingredients: ', ingr)
-            print('instructions: ', instr)
+        print('id: ', id)
+            #print('name: ', n)
+            #print('ingredients: ', ingr)
+            #print('instructions: ', instr)"""
 
 obj = Recipes()
 
-obj.get_basics()
+obj.get_ID()
